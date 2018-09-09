20a11e72c91497db97d5dc37daf2a112e9563ea2
Luigi <luigiagcaoili@gmail.com>
2018-09-08 22:29:09 -0400

    Update better-indeed.user.js
    - Improved sponsored posts removal
    - Moved sponsored and Job Spotter post toggles to the side

--- better-indeed.user.js
+++ better-indeed.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             better-indeed
 // @name           Better Indeed
-// @version        1.0.1
+// @version        1.0.3
 // @namespace      https://github.com/luigia
 // @author         Luigi Agcaoili
 // @license        MIT - https://opensource.org/licenses/MIT
@@ -13,15 +13,27 @@
 // @grant          none
 // ==/UserScript==
 
-// select & edit the top bar
-let bar = document.querySelector('#resumePromo') || document.querySelector('.resultsTop')
+// select & edit the side bar
+const filters = document.querySelector('#refineresults'),
+  bar = document.createElement('div')
 
 bar.innerHTML = `
+  <h4 class="toggle-title">Toggle ads</h4>
   <input type="checkbox" id="job-spotter-checkbox">
   <label for="job-spotter-checkbox">Hide Job Spotter ads</label>
   <input type="checkbox" id="sponsored-checkbox">
   <label for="sponsored-checkbox">Hide sponsored ads</label>
-`
+  `
+bar.style.marginBottom = '24px'
+bar.style.marginLeft = '0'
+bar.style.paddingLeft = '24px'
+bar.style.color = '#2d2d2d'
+bar.style.fontSize = '12px'
+bar.style.maxWidth = '70%'
+bar.firstElementChild.style.fontSize = '14px'
+bar.firstElementChild.style.fontWeight = '500'
+bar.firstElementChild.style.color = '#000'
+filters.insertBefore(bar, filters.firstChild)
 
 // checkbox variables
 const jobSpotCb = document.querySelector('#job-spotter-checkbox'),
@@ -46,12 +58,12 @@ window.onload = () => {
   }
 
   // move pagination to the side
-  const pages = document.querySelector('.pagination'),
+  const pagination = document.querySelector('.pagination'),
     results = document.querySelector('#resultsCol')
   // delay as job postings aren't instantly loaded
   setTimeout(() => {
     let side = document.querySelector('#jobalerts') || document.querySelector('#vjs-content')
-    side.insertBefore(pages, side.firstChild)
+    side.insertBefore(pagination, side.firstChild)
   }, 200)
 
   // move pagination back to the side if it's removed
@@ -59,7 +71,7 @@ window.onload = () => {
     setTimeout(() => {
       if (document.querySelector('#vjs-content')) {
         let vjsContent = document.querySelector('#vjs-content')
-        vjsContent.insertBefore(pages, vjsContent.firstChild)
+        vjsContent.insertBefore(pagination, vjsContent.firstChild)
       }
 
       if (document.querySelector('#vjs-x')) {
@@ -67,7 +79,7 @@ window.onload = () => {
         close.addEventListener('click', (e) => {
           if (document.querySelector('#jobalerts')) {
             side = document.querySelector('#jobalerts')
-            side.insertBefore(pages, side.firstChild)
+            side.insertBefore(pagination, side.firstChild)
           }
         })
       }
@@ -93,13 +105,25 @@ const hideJobSpot = () => {
 
 // hide sponsored postings
 const hideSponsored = () => {
-  let sponsored = Array.from(document.querySelectorAll('.sponsoredGray'))
+  let lastSponsored = Array.from(document.querySelectorAll('.sjlast'))
 
   if (sponsoredCb.checked) {
     localStorage.setItem(sponsoredCb.id, sponsoredCb.checked)
-    sponsored.forEach((ad) => ad.parentElement.parentElement.parentElement.parentElement.style.display = 'none')
+    if (lastSponsored.length === 1) {
+      lastSponsored[0].parentElement.style.display = 'none'
+    } else {
+      lastSponsored.forEach((section) => {
+        section.parentElement.style.display = 'none'
+      })
+    }
   } else {
     localStorage.removeItem(sponsoredCb.id)
-    sponsored.forEach((ad) => ad.parentElement.parentElement.parentElement.parentElement.style.display = 'block')
+    if (lastSponsored.length === 1) {
+      lastSponsored[0].parentElement.style.display = 'block'
+    } else {
+      lastSponsored.forEach((section) => {
+        section.parentElement.display = 'block'
+      })
+    }
   }
 }
